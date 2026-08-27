fec3a2317057e969a924a3bb8daaebedb87c5566
solrac031ch-prog <solrac031.ch@gmail.com>
2026-08-27 12:03:02 -0400

    Mostrar precios diarios de Farmacia Popular Cerro Navia (#29)
    * Add daily Farmacia Popular price layer
    
    * Style Farmacia Popular price availability panel
    
    * Seed Farmacia Popular Cerro Navia data file
    
    * Add daily Cerro Navia pharmacy scraper
    
    * Add daily Farmacia Popular Cerro Navia sync
    
    * Load Farmacia Popular daily price layer
    
    * Validate Farmacia Popular integration

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_NAME = "insulog-shell-20260827-atomic12";
-const DEPLOYMENT_REVISION = "empagliflozina-card-20260827-r2";
+const DEPLOYMENT_REVISION = "farmacia-popular-20260827-r3";
 
 const APP_SHELL = [
   "./index.html",
@@ -9,10 +9,12 @@ const APP_SHELL = [
   "./pdf-enhancements.css?v=20260827-3",
   "./pdf-enhancements.css?v=20260827-4",
   "./aps-safety-2026.css?v=20260827-2",
+  "./farmacia-popular.css?v=20260827-1",
   "./app.js?v=20260826",
   "./pdf-enhancements.js?v=20260827-3",
   "./pdf-enhancements.js?v=20260827-4",
   "./aps-safety-2026.js?v=20260827-2",
+  "./farmacia-popular.js?v=20260827-1",
   "./manifest.webmanifest?v=20260826",
   "./assets/icons/icon-32.png?v=20260826",
   "./assets/icons/icon-180.png?v=20260826",
@@ -34,7 +36,9 @@ async function normalizarAssetClinico(request, response) {
     'label: "Empagliflozina"'
   );
 
-  return new Response(normalizado, {
+  const farmaciaLoader = `\n;(() => {\n  if (!document.querySelector('link[data-farmacia-popular]')) {\n    const link = document.createElement('link');\n    link.rel = 'stylesheet';\n    link.href = './farmacia-popular.css?v=20260827-1';\n    link.dataset.farmaciaPopular = 'true';\n    document.head.appendChild(link);\n  }\n  if (!document.querySelector('script[data-farmacia-popular]')) {\n    const script = document.createElement('script');\n    script.src = './farmacia-popular.js?v=20260827-1';\n    script.dataset.farmaciaPopular = 'true';\n    document.body.appendChild(script);\n  }\n})();\n`;
+
+  return new Response(normalizado + farmaciaLoader, {
     status: response.status,
     statusText: response.statusText,
     headers: response.headers
