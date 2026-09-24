55b1d0d0455e48a7e46a1f3d8fed9bd693cecd62
solrac031ch-prog <solrac031.ch@gmail.com>
2026-09-24 15:39:37 -0300

    Bump compact UI sync version

--- phase6b-document-sync.js
+++ phase6b-document-sync.js
@@ -962,7 +962,7 @@
   }
 
   window.InsulogPhase6BDocumentSync = Object.freeze({
-    version: "2026.09.24-phase6b-document-sync-prepilot-v7",
+    version: "2026.09.24-phase6b-document-sync-ui-v8",
     configureDriveEndpoint,
     driveStatus: () => Object.freeze({
       configured: Boolean(configuredDriveEndpoint()),
